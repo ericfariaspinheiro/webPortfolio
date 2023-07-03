1d83f8ab1f2c52c8911265212157d0ee76c14539
eric.pinheiro <eric.pinheiro@agenciam3.com>
2023-07-03 18:50:21 -0300

    ajustes gerais

--- src/pages/Home/About.js
+++ src/pages/Home/About.js
@@ -12,10 +12,9 @@ export default function About() {
             </h3>
             <br />
             <p>
-              {" "}
-              I have always been interested in how technologies work, especially the web. Getting to know its ins and outs and working to push it even
-              further is my goal. Previously I worked with API documentation and Testing Automation, but now I am aiming to make a career shift to
-              software development.
+              Experienced professional in technical documentation and software testing, with a strong background in APIs and best practices. Recently
+              made a transition to front-end development, accumulating 1 year of experience as a front-end developer. Specialized in React,
+              JavaScript, TypeScript, SASS, and VTEX.
             </p>
             <div className="contactButton">
               <a href="#contact">Contact me!</a>
